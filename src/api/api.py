aa02324661862dde2c3480e58cfa994ff13c394a
Lauri Huhta <huhta.lauri@gmail.com>
2022-08-16 21:33:17 +0300

    api structure ready

---
+++ src/api/api.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from flask import Flask, request
+
+HOST = '0.0.0.0' if os.getenv('HOSTNAME') == 'api' else '127.0.0.1'
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Welcome to the api"
+
+
+@app.route('/conveyor', methods=["POST"])
+def conveyor():
+    task = json.loads(request.data)
+    return task
+
+
+app.run(host=HOST, port=9999, debug=True)
+
+
+#curl -X POST 127.0.0.1:9999/conveyor    -H 'Content-Type: application/json'    -d '{"login":"my_login","password":"my_password"}'
